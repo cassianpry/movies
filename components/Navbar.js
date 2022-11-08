057bacfd645b3565211f70d5827eed0f6a267c2d
cassianpry <crmarinello@gmail.com>
2022-11-08 12:26:42 -0300

    Criado o componente Navbar e requests.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+import requests from '../utils/requests';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-center bg-gray-600 text-gray-200 text-xl lg:text-2xl select-none">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <Link href={`?genre=${key}`} key={key}>
+          <h2 className="m-6 hover:text-white active:text-red-400">{title}</h2>
+        </Link>
+      ))}
+    </div>
+  );
 }
