e1dc806b6b5d9ce8e25b1e05a09bde2d7c70043f
cassianpry <crmarinello@gmail.com>
2022-11-08 11:33:25 -0300

    Criado componente Header e HeaderIcon

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+import HeaderIcon from './HeaderIcon';
+import {
+  HomeIcon,
+  InformationCircleIcon,
+  PhoneIcon,
+  UserIcon,
+} from '@heroicons/react/24/solid';
+
 export default function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <div className="">
+        <HeaderIcon Icon={HomeIcon} title="Home" />
+        <HeaderIcon Icon={UserIcon} title="Account" />
+        <HeaderIcon Icon={PhoneIcon} title="Contact" />
+        <HeaderIcon Icon={InformationCircleIcon} title="About" />
+      </div>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/250px-IMDB_Logo_2016.svg.png"
+        alt="imdb logo"
+        width={100}
+        height={100}
+      />
     </div>
   );
 }
